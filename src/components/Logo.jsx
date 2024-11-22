123da874095d194125fa9aa806b9ba9b13366872
banuaggun <banuaggun@outlook.com>
2024-11-22 15:38:26 +0300

    A general arrangement was made.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,17 +3,13 @@ import '../assets/styles/logo.css'
 
 function Logo() {
 
-  const handleToggleSidebar = () => {
-    document.body.classList.toggle('toggle-sidebar')
-  }
   return (
     <div className='logo-area'>
       <a href='/' className="logo-area-content">
-        {/* <img src="" alt="" />*/}
         <i className="ph ph-diamonds-four"></i>
         <span>Admin Dashboard</span>
       </a>
-      <i className='ph ph-funnel-simple toggle-sidebar-btn' onClick={handleToggleSidebar}></i>
+    
     </div>
   )
 }
