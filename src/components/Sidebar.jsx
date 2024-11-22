611ece6f198fab06fc07af105211877a31f6d371
banuaggun <banuaggun@outlook.com>
2024-11-22 15:41:03 +0300

    A sidebar with working functionality has been created.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from 'react';
+import '../assets/styles/sidebar.css';
+
+const Sidebar = () => { 
+  
+  const [isOpen, setIsOpen] = useState(false); 
+  const sidebarRef = useRef(null); 
+  
+  const toggleSidebar = () => { setIsOpen(!isOpen); };
+  
+  const handleClickOutside = (event) => { 
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) { 
+      setIsOpen(false); 
+    } 
+  }; 
+  
+  useEffect(() => { 
+    document.addEventListener('mousedown', handleClickOutside); 
+    return () => { 
+      document.removeEventListener('mousedown', handleClickOutside); 
+    }; 
+  }, []); 
+  
+  return ( 
+    <div className="sidebar-container"> 
+      <button className="toggle-button" onClick={toggleSidebar} ref={sidebarRef}> 
+        <i className='ph ph-funnel-simple'></i> 
+      </button> 
+      <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}> 
+        <ul> 
+          <li>Home</li> 
+          <li>About</li> 
+          <li>Services</li> 
+          <li>Contact</li> 
+        </ul> 
+      </div> 
+    </div> 
+  ); 
+};
+
+export default Sidebar
