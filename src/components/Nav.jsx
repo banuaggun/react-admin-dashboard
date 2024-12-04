c15433218c09ef667ee49f33e4db2e5ca2b93426
banuaggun <banuaggun@outlook.com>
2024-12-04 14:26:07 +0300

    Some positioning and font size values ​​have been readjusted.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,14 @@ import '../assets/styles/nav.css'
 import NavNotice from './NavNotice'
 import NavMessage from './NavMessage'
 import NavUser from './NavUser'
-import Sidebar from './Sidebar'
 
 function Nav() {
   return (
     <nav>
-      <div className='nav-left'>
-        <Sidebar/>
-      </div>
-      <div className='nav-right'>
+     
         <NavNotice />
         <NavMessage />
         <NavUser />
-      </div>
     </nav>
   )
 }
