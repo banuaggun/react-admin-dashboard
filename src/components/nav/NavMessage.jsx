029bbcc84e23554e42af37e071e8b0f45c5303dc
banuaggun <banuaggun@outlook.com>
2024-12-17 21:45:47 +0300

    Badges have been added to message and notification icons.

--- src/components/nav/NavMessage.jsx
+++ src/components/nav/NavMessage.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Dropdown from  '../../functions/Dropdown'
-import data from '../../data/data.json'
+import React from "react";
+import Dropdown from "../../functions/Dropdown";
+import data from "../../data/data.json";
+import './nav.css'
 
 function NavMessage() {
-  return (
-
-    <div>
-      <Dropdown icon={<i className="ph ph-chat"></i>} items={data.messages.map(message => ({ title: message.sender, text: message.content, time: message.time }))} />
-    </div>
-  )
+    return (
+        <div>
+            <Dropdown
+                icon={
+                    <div className="icon-container">
+                        <i className="ph ph-chat"></i>
+                        <span className="badge badge-message">2</span>
+                    </div>
+                }
+                items={data.messages.map((message) => ({
+                    title: message.sender,
+                    text: message.content,
+                    time: message.time,
+                }))}
+            />
+        </div>
+    );
 }
 
-export default NavMessage
+export default NavMessage;
--- src/components/nav/NavNotice.jsx
+++ src/components/nav/NavNotice.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Dropdown from '../../functions/Dropdown'
-import data from '../../data/data.json'
+import React from "react";
+import Dropdown from "../../functions/Dropdown";
+import data from "../../data/data.json";
+import "./nav.css";
 
 const NavNotice = () => {
-  return (
-    <div>
-  <Dropdown icon={<i className="ph ph-bell"></i>} items={data.notifications.map(notification => ({ title: notification.title, text: notification.text, time: notification.time }))} />
-    
-    </div>
-  )
-}
+    return (
+        <div>
+            <Dropdown
+                icon={
+                    <div className="icon-container">
+                        <i className="ph ph-bell"></i>
+                        <span className="badge badge-notice">4</span>
+                    </div>
+                }
+                items={data.notifications.map((notification) => ({
+                    title: notification.title,
+                    text: notification.text,
+                    time: notification.time,
+                }))}
+            />
+        </div>
+    );
+};
 
-export default NavNotice
+export default NavNotice;
