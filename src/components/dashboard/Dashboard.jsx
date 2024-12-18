40643285138989e69be07bda7a274c7e6c5c6a3e
banuaggun <banuaggun@outlook.com>
2024-12-18 21:48:06 +0300

    Data retrieval was done with Fetch.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Card from '../card/Card'
 import './dashboard.css'
 
 const Dashboard = () => {
+
+  const [cards, setCards] = useState([])
+
+  const fetchData = () => {
+    fetch('../api/info.json').then(res => res.json()).then(data => {
+      setCards(data.cards);
+    }).catch(e => console.log(e.message))
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section className="dashboard section">
       <div className="row">
         <div className="col-lg-8">
-          <div className="row"></div>
+          <div className="row">
+            {
+              cards && cards.length > 0 && cards.map(card => <Card key={card._id} card={card} />)
+            }
+          </div>
         </div>
         <div className="col-lg-4"></div>
       </div>
