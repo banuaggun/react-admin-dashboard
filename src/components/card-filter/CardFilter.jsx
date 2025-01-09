b16f7f6b60c6bd9db146ef95cf59ec9ea07222ae
banuaggun <banuaggun@outlook.com>
2025-01-09 15:58:12 +0300

    Card filtering element is defined as dropdown menu

--- src/components/card-filter/CardFilter.jsx
+++ src/components/card-filter/CardFilter.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
+import Dropdown from '../../functions/Dropdown'
+import data from '../../data/data.json'
 
-const CardFilter = ({filterChange}) => {
+const CardFilter = () => {
+  console.log(data.filters); // Bu satırı ekleyin
   return (
+    
     <div className='filter'>
-      <a href="#" className="icon" data-bs-toggle="dropdown">
-        <i className='ph ph-bell'></i>
-      </a>
-      <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-        <li className="dropdown-header text-start">
-          <h6>Filter</h6>
-        </li>
-        <li>
-          <a className="dropdown-item" onClick={() => filterChange('Today')}>
-            Today
-          </a>
-        </li>
-        <li>
-          <a className="dropdown-item" onClick={() => filterChange('This Month')}>
-            This Month
-          </a>
-        </li>
-        <li>
-          <a className="dropdown-item" onClick={() => filterChange('This Year')}>
-            This Year
-          </a>
-        </li>
-      </ul>
+      <Dropdown
+                icon={
+                    <div className="icon-container">
+                        <i className="ph ph-bell"></i>
+                    </div>
+                }
+                items={data.filters.map((filter) => ({
+                  name: filter.name
+                }))}
+            />
+     
+   
     </div>
   )
 }
