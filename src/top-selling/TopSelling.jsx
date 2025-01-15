c0d9443cc8e60987cc8c733c772ad580ef4ef7c9
banuaggun <banuaggun@outlook.com>
2025-01-15 21:54:50 +0300

    Top selling area completed.

--- src/top-selling/TopSelling.jsx
+++ src/top-selling/TopSelling.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import CardFilter from '../components/card-filter/CardFilter';
 import './topselling.css'
+import TopSellingItem from './TopSellingItem';
 
 const TopSelling = () => {
   const [items, setItems] = useState([]);
@@ -24,6 +25,20 @@ const TopSelling = () => {
         <h5 className="card-title">
           <span>Top Selling | {filter}</span>
         </h5>
+        <table className='table table-borderless'>
+          <thead className='table-light'>
+            <tr>
+              <th scope='col'>Preview</th>
+              <th scope='col'>Product</th>
+              <th scope='col'>Price</th>
+              <th scope='col'>Sold</th>
+              <th scope='col'>Revenue</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items && items.length > 0 && items.map(item => <TopSellingItem key={item._id} item={item} />)}
+          </tbody>
+        </table>
       </div>
     </div>
   )
