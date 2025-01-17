24a22bcf9d8829bff57c7ff83f9ab302c0e95731
banuaggun <banuaggun@outlook.com>
2025-01-17 16:52:57 +0300

    Recent Activity data extraction process has been completed and style properties have been added.

--- src/components/recent-activity/RecentActivityItem.jsx
+++ src/components/recent-activity/RecentActivityItem.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './recentactivity.css'
 
-const RecentActivityItem = () => {
+const RecentActivityItem = ({item}) => {
   return (
-    <div>RecentActivityItem</div>
+    <div className="activity-item d-flex">
+      <div className="active-label">
+        {item.time}
+      </div>
+      <i className={`ph ph-bell activity-badge ${item.color} align-self-start`}></i>
+      {item.highlight === '' ? (
+        <div className='activity-content'>{item.content}</div>
+      ) : (
+        <div className='activity-content'>
+          {item.content.substring(0, item.content.indexOf(item.highlight))}
+          <a href='#' className='fw-bold text-dark'>
+            {item.highlight}
+          </a>
+          {item.content.slice(item.content.indexOf(item.highlight) + item.highlight.length, -1)}
+        </div>
+      )}
+    </div>
   )
 }
 
