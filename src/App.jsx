8441724f93c863ffc8ee658ac6d69b1d7b5c10e7
banuaggun <banuaggun@outlook.com>
2025-01-29 23:42:26 +0300

    dashboard style studies - 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import Header from './components/header/Header'
 import './App.css'
 import MainArea from './components/main-area/MainArea'
+import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
 
   return (
     <>
-     
+     <div className="content-header">
       <Header />
-      <MainArea/>
+     </div>
+     <div className="area">
+      <div className="content-sidebar">
+          <Sidebar/>
+        </div>
+        <div className="content-main">
+          <MainArea/>
+        </div>
+     </div>
+      
+      
     </>
   )
 }
