038f8f01bea2bd11377f4c3d2dbfbee0d1e20cc2
Banu Ağgün <banuaggun@outlook.com>
2025-02-20 14:54:46 +0300

    The content movement is made according to the Sidebar.

--- src/components/dashboard/Blank.jsx
+++ src/components/dashboard/Blank.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import './dashboard.css'
+import { useState, useEffect } from 'react';
+import './dashboard.css';
+import Card from '../card/Card';
+import RecentActivity from '../recent-activity/RecentActivity';
 
-const Blank = () => {
+const Blank = ({ isExpanded }) => {
+  const [cards, setCards] = useState([]);
+
+  const fetchData = () => {
+    fetch('../api/info.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data.cards);
+      })
+      .catch((e) => console.log(e.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log("isExpanded:", isExpanded); // Debugging için eklendi
+
   return (
-    <section aria-label='dashboard' className='dashboard-section'>
-      <div className="row"></div>
+    <section aria-label="dashboard" className={`dashboard ${isExpanded ? 'sidebar-close' : 'sidebar-open'}`}>
+      <div className="dashboard-row">
+        <div className="dashboard-row-left">
+          <div className="dashboard-row-left-card">
+            {cards && cards.length > 0 && cards.map((card) => <Card key={card._id} card={card} />)}
+          </div>
+        </div>
+        <div className="dashboard-row-right">
+          <RecentActivity />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Blank
+export default Blank;
