a163284bea91241289fe6ba1fac7ea870eaae6b6
Banu Ağgün <banuaggun@outlook.com>
2025-02-20 14:53:04 +0300

    Sidebar movement interaction confirmed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import Header from './components/header/Header'
-import './App.css'
-import MainArea from './components/main-area/MainArea'
-import Sidebar from './components/sidebar/Sidebar'
+import React, { useState } from 'react';
+import Header from './components/header/Header';
+import './App.css';
+import MainArea from './components/main-area/MainArea';
+import Sidebar from './components/sidebar/Sidebar';
 
-function App({isExpanded}) {
+function App() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <>
-     <div className="content-header">
-      <Header />
-     </div>
-     <div className="area">
-      <div className="content-sidebar">
-          <Sidebar/>
+      <div className="content-header">
+        <Header />
+      </div>
+      <div className="area">
+        <div className="content-sidebar">
+          <Sidebar isExpanded={isExpanded} toggleSidebar={toggleSidebar} />
         </div>
         <div className={isExpanded ? "content-main" : "content-main-out"}>
-          <MainArea/>
+          <MainArea isExpanded={isExpanded} />
         </div>
-     </div>
-      
-      
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
