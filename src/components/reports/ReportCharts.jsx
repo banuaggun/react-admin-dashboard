15b35988793fabcb946eea9cea460d765a366118
Banu Ağgün <banuaggun@outlook.com>
2025-02-25 21:17:06 +0300

    The report chart area has been edited.

--- src/components/reports/ReportCharts.jsx
+++ src/components/reports/ReportCharts.jsx
@@ -1,74 +1,78 @@
-import {useState} from 'react'
-import Chart from 'react-apexcharts'
+import { useState } from "react";
+import Chart from "react-apexcharts";
 
 function ReportCharts() {
   const [data, setData] = useState({
-    series:[
+    series: [
       {
-        name:'Sales',
-        data:[30, 40, 28, 51, 42, 82, 56]
+        name: "Sales",
+        data: [30, 40, 28, 51, 42, 82, 56],
       },
       {
-        name:'Revenue',
-        data:[11, 32, 45, 32, 34, 52, 41]
+        name: "Revenue",
+        data: [11, 32, 45, 32, 34, 52, 41],
       },
       {
-        name:'Customers',
-        data:[15, 11, 32, 18, 9, 24, 11]
-      }
+        name: "Customers",
+        data: [15, 11, 32, 18, 9, 24, 11],
+      },
     ],
-    options:{
-      chart:{
-        height:350,
-        type:'area',
-        toolbar:{
-          show:false,
+    options: {
+      chart: {
+        height: 350,
+        type: "area",
+        toolbar: {
+          show: false,
         },
       },
-      markers:{
-        size:4,
+      markers: {
+        size: 4,
       },
-      colors:['#4154f1', '#2eca6a', '#ff771d'],
-      fill:{
-        type:'gradient',
-        gradient:{
-          shadeIntensity:1,
-          opacityForm:0.3,
-          opacityTo:0.4,
-          stops:[0, 90, 100],
+      colors: ["#4154f1", "#2eca6a", "#ff771d"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityForm: 0.3,
+          opacityTo: 0.4,
+          stops: [0, 90, 100],
         },
       },
-      dataLabels:{
-        enabled:false,
+      dataLabels: {
+        enabled: false,
       },
-      stroke:{
-        curve:'smooth',
-        width:2,
+      stroke: {
+        curve: "smooth",
+        width: 2,
       },
-      xaxis:{
-        type:'datetime',
-        categories:[
-          '2024-10-20T00:00:00.00Z',
-          '2024-10-20T01:30:00.00Z',
-          '2024-10-20T02:30:00.00Z',
-          '2024-10-20T03:30:00.00Z',
-          '2024-10-20T04:30:00.00Z',
-          '2024-10-20T05:30:00.00Z',
-          '2024-10-20T06:30:00.00Z'
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2024-10-20T00:00:00.00Z",
+          "2024-10-20T01:30:00.00Z",
+          "2024-10-20T02:30:00.00Z",
+          "2024-10-20T03:30:00.00Z",
+          "2024-10-20T04:30:00.00Z",
+          "2024-10-20T05:30:00.00Z",
+          "2024-10-20T06:30:00.00Z",
         ],
       },
-      tooltip:{
-        x:{
-          format:'dd/MM/yy HH:mm',
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
         },
       },
     },
   });
 
   return (
-    <Chart options={data.options} series={data.series} type={data.options.chart.type} height={data.options.chart.height} />
-    
-  )
+    <Chart
+      options={data.options}
+      series={data.series}
+      type={data.options.chart.type}
+      height={data.options.chart.height}
+    />
+  );
 }
 
-export default ReportCharts
+export default ReportCharts;
--- src/components/reports/Reports.jsx
+++ src/components/reports/Reports.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './reports.css'
 import CardFilter from '../card-filter/CardFilter'
 import ReportCharts from './ReportCharts';
 
@@ -9,13 +10,24 @@ const Reports = () => {
   };
 
   return (
-    <div className='card'>
-      <CardFilter filterChange={handleFilterChange} />
-      <div className="card-body">
-        <h6 className="card-title">
-          <span>Reports/{filter}</span>
-        </h6>
-        <ReportCharts/>
+    <div className='reports'>
+      <div className="reports-content">
+
+        <div className="reports-content-filter">
+          <CardFilter filterChange={handleFilterChange} />
+        </div>
+
+        <div className="reports-content-body">
+
+          <div className="reports-title">
+            <span>Reports/{filter}</span>
+          </div>
+
+          <div className="reports-chart">
+            <ReportCharts/>
+          </div>
+
+        </div>
       </div>
     </div>
   )
