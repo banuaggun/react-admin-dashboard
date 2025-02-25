5ce96d3a9aed9cb0b332397477517c2c0079148b
Banu Ağgün <banuaggun@outlook.com>
2025-02-25 21:18:09 +0300

    Minor adjustments were made.

--- src/components/dashboard/Blank.jsx
+++ src/components/dashboard/Blank.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import './dashboard.css';
 import Card from '../card/Card';
 import RecentActivity from '../recent-activity/RecentActivity';
+import Reports from '../reports/Reports';
+import RecentSales from '../recent-sales/RecentSales';
+import TopSelling from '../../top-selling/TopSelling';
 
 const Blank = ({ isExpanded }) => {
   const [cards, setCards] = useState([]);
@@ -28,9 +31,20 @@ const Blank = ({ isExpanded }) => {
           <div className="dashboard-row-left-card">
             {cards && cards.length > 0 && cards.map((card) => <Card key={card._id} card={card} />)}
           </div>
+          <div className="dashboard-row-left-sales">
+            <Reports/>
+          </div>
         </div>
         <div className="dashboard-row-right">
           <RecentActivity />
+        </div>
+      </div>
+      <div className="dashboard-row">
+        <div className="dashboard-row-left">
+          <RecentSales/>
+        </div>
+        <div className="dashboard row-right">
+          <TopSelling/>
         </div>
       </div>
     </section>
