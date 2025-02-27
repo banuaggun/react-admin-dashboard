f4e5fc8f0ca6385c4923546a5e8d8bd192062e62
Banu Ağgün <banuaggun@outlook.com>
2025-02-27 21:04:43 +0300

    Top selling data was tabulated.

--- src/top-selling/TopSellingItem.jsx
+++ src/top-selling/TopSellingItem.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
-import './topselling.css'
+import './topsellingitem.css'
 
-const TopSellingItem = ({item}) => {
+const TopSellingItem = ({items}) => {
   return (
-    <tr>
-      <th scope='row'>
-        <a href='#'>
-          <img className='top-selling-img' src={item.preview} alt="" />
-        </a>
-      </th>
-      <td>
-        <a href='#' className='text-primary fw-bold'>
-          {item.name}
-        </a>
-      </td>
-      <td>${item.price.toFixed(2)}</td>
-      <td className="fw-bold">{item.sold}</td>
-      <td>${(item.price * item.sold).toLocaleString('en-US')}</td>
-    </tr>
+    <table className="top-selling-table">
+      <thead className='top-selling-table-header'>
+        <tr>
+          <th scope='col'>Preview</th>
+          <th scope='col'>Product</th>
+          <th scope='col'>Price</th>
+          <th scope='col'>Sold</th>
+          <th scope='col'>Revenue</th>
+        </tr>
+      </thead>
+      <tbody>
+      {items && items.length > 0 && items.map((item) => (
+        <tr key={item.id}>
+          <th scope='row'>
+          <a href='#'>
+            <img className='top-selling-img' src={item.preview} alt="" />
+          </a>
+          </th>
+          <td data-label="product" scope='row'>
+            <a href='#' className='text-primary fw-bold'>
+              {item.name}
+            </a>
+          </td>
+          <td data-label="price" scope='row'>
+            ${item.price.toFixed(2)}
+          </td>
+          <td data-label="sold" scope='row' className="fw-bold">
+            {item.sold}
+          </td>
+          <td data-label="revenue" scope='row'>
+            ${(item.price * item.sold).toLocaleString('en-US')}  
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+
   )
 }
 
