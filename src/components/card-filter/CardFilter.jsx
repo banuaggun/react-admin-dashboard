90bb23fce45c7e9d0e7e4436fabc80a449b12158
Banu Ağgün <banuaggun@outlook.com>
2025-05-14 20:41:51 +0300

    Necessary arrangements have been made

--- src/components/card-filter/CardFilter.jsx
+++ src/components/card-filter/CardFilter.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Dropdown from '../../functions/Dropdown'
-import data from '../../data/data.json'
+import React, { useState, useEffect } from 'react';
+import Dropdown from '../../functions/dropdown/Dropdown';
 
 const CardFilter = () => {
-  console.log(data.filters); // Bu satırı ekleyin
+  const [filters, setFilters] = useState([]);
+
+  // Verileri çekmek için fetch fonksiyonu
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/info.json');
+        if (!response.ok) {
+          throw new Error('Veri çekme başarısız oldu');
+        }
+        const data = await response.json();
+        setFilters(data.filters); // Gelen veriyi state'e kaydet
+        console.log(data.filters); // Debug için loglama
+      } catch (error) {
+        console.error('Hata:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    
     <div className='filter'>
       <Dropdown
-                icon={
-                    <div className="icon-container">
-                        <i className="ph-fill ph-circles-three-plus"></i>
-                    </div>
-                }
-                items={data.filters.map((filter) => ({
-                  name: filter.name
-                }))}
-            />
-     
-   
+        icon={
+          <div className="icon-container">
+            <i className="ph-fill ph-circles-three-plus"></i>
+          </div>
+        }
+        items={filters.map((filter) => ({
+          name: filter.name
+        }))}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default CardFilter
+export default CardFilter;
