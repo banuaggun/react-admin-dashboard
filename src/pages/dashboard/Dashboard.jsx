b683638c20948c4776b9985ce0cce84e1ff6556c
Banu Ağgün <banuaggun@outlook.com>
2025-05-16 00:07:37 +0300

    Necessary definitions were made for the dashboard and it was made responsive.

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import {useEffect, useState} from "react";
+import "./dashboard.css";
+import PageTitle from "../../components/page-title/Pagetitle";
+import Card from "../../components/card/Card";
+import RecentActivity from "../../components/recent-activity/RecentActivity";
+import Reports from "../../components/reports/Reports";
+import RecentSales from "../../components/recent-sales/RecentSales";
+import Topselling from "../../components/topselling/Topselling";
 
-const Dashboard = () => {
+const Dashboard = ({ isExpanded }) => {
+  const [cards, setCards] = useState([]);
+
+  const fetchData = () => {
+    fetch("../api/info.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data.cards);
+      })
+      .catch((e) => console.log(e.message));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+ 
   return (
-    <div>Dashboard</div>
-  )
-}
+    <section aria-label="dashboard" className={`dashboard ${isExpanded ? "sidebar-close" : "sidebar-open"}`}>
+      <div className="row-1">
+        <PageTitle page="dashboard" />
+      </div>
+      <div className="row-2">
+        <div className="col-1">
+          {cards && cards.length > 0 && cards.map((card) => <Card key={card._id} card={card} />)}
+        </div>
+        <div className="col-2">
+          <RecentActivity />
+        </div>
+      </div>
 
-export default Dashboard
+      <div className="row-3">
+        <div className="col-1">
+          <Reports />
+        </div>
+      
+        <div className="col-2">
+          <RecentSales />
+        </div>
+        
+      </div>
+    
+      <div className="row-4">
+        <div className="col-1">
+          <Topselling/>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Dashboard;
