5bbe89a6561788ca6ab807fa810591d06c8836b5
Banu Ağgün <banuaggun@outlook.com>
2025-05-28 23:08:09 +0300

    data was called with customHook structure and active class was assigned

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,41 +1,51 @@
-import { useEffect, useState } from 'react'
-import './sidebar.css'
-import { Link } from 'react-router-dom'
-
+import { useState } from "react";
+import "./sidebar.css";
+import { Link, useLocation } from "react-router-dom";
+import useFetchData from "../../functions/hooks/FetchData"; // Hook'u import et
 
 const Sidebar = () => {
-  
-  const [isExpanded, setIsExpanded] = useState(false)
-  const [items, setItems] = useState([])
-
-  const fetchData = async () => {
-    try{
-      const res = await fetch('../api/info.json');
-      const data = await res.json();
-      setItems(data.sidebar);
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const { data: items, loading, error } = useFetchData("/api/info.json"); // Hook'u kullan
+  const location = useLocation(); // Mevcut URL'yi al
 
   return (
-    <>
-    <div className={isExpanded ? "side-nav-container" : "side-nav-container side-nav-container-nx"}>
+    <div
+      className={
+        isExpanded
+          ? "side-nav-container"
+          : "side-nav-container side-nav-container-nx"
+      }
+    >
       <div className="nav-upper">
         <div className="nav-heading">
-          
-          <button className={isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"} onClick={() => setIsExpanded(!isExpanded)}>
-            <i className={isExpanded ? 'ph-fill ph-caret-left' : 'ph ph-funnel-simple'}></i>
+          <button
+            className={
+              isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
+            }
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            <i
+              className={
+                isExpanded ? "ph-fill ph-caret-left" : "ph ph-funnel-simple"
+              }
+            ></i>
           </button>
         </div>
       </div>
       <div className="nav-menu">
-        {items.map(({id, icon, text}) => (
-          <Link to={text} key={id} className={isExpanded ? "menu-item" : "menu-item menu-item-nx"}>
+        {items?.sidebar?.map(({ id, icon, text }) => (
+          <Link
+            to={`/${text}`}
+            key={id}
+            className={`menu-item ${isExpanded ? "" : "menu-item-nx"} 
+              ${
+                location.pathname === `/${text}` ||
+                (location.pathname === "/" &&
+                  text.toLowerCase().includes("dashboard"))
+                  ? "active"
+                  : ""
+              }`}
+          >
             <i className={icon}></i>
             {isExpanded && <span>{text}</span>}
             {!isExpanded && <p>{text}</p>}
@@ -43,9 +53,7 @@ const Sidebar = () => {
         ))}
       </div>
     </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
