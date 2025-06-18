f7733b76f0766dbd8c39c28f44e77550b6c31a0f
Banu Ağgün <banuaggun@outlook.com>
2025-06-18 14:44:50 +0300

    Responsive table created

--- src/components/customer-elements/customer-table/CustomerTable.jsx
+++ src/components/customer-elements/customer-table/CustomerTable.jsx
@@ -5,11 +5,10 @@ const CustomerTable = ({ items }) => {
 
   return (
       <table className="customer-table">
-        <thead className="customer-table-head">
+        <thead className="customer-table-header">
           <tr>
             <th scope="col">Name</th>
             <th scope="col">Amount</th>
-            <th scope="col">Product</th>
             <th scope="col">Payment</th>
             <th scope="col">Installments</th>
           </tr>
@@ -17,19 +16,16 @@ const CustomerTable = ({ items }) => {
         <tbody>
           {items?.length > 0 && items.map((item) => (
             <tr key={item.id}>
-              <td scope="row">
+              <td data-label="name" scope="row">
                 <span>{item?.customer_name}</span>
               </td>
               <td data-label="spending" scope="row">
-                {item?.spending}
+                ${item?.spending}
               </td>
-              <td data-label="product" scope="row">
-                {item?.product}
-              </td>
-              <td data-label="payment_type" scope="row">
+              <td data-label="payment type" scope="row">
                 {item?.payment_type}
               </td>
-              <td data-label="installments_left" scope="row">
+              <td data-label="installments left" scope="row">
                 {item?.installments_left}
               </td>
             </tr>
