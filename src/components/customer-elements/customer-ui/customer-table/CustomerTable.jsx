54b56cfe687cb1a62860000ec185be09be3ed27d
Banu Ağgün <banuaggun@outlook.com>
2025-07-02 00:19:25 +0300

    customer table and positioning reorganized

--- src/components/customer-elements/customer-ui/customer-table/CustomerTable.jsx
+++ src/components/customer-elements/customer-ui/customer-table/CustomerTable.jsx
@@ -1,63 +1,67 @@
 import React, { useState } from "react";
 import CustomerDetail from "../customer-detail/CustomerDetail";
 import Pagination from "../pagination/Pagination";
+import PersonalInfo from "../customer-actions/PersonalInfo";
 import './customer-table.css';
 
 const CustomerTable = ({ items }) => {
-  const [detail, setDetail] = useState('Personal Information');
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
 
-  const handleDetailChange = detail => {
-    setDetail(detail);
-  };
+  const [selectedCustomerId, setSelectedCustomerId] = useState(null);
+  const [selectedAction, setSelectedAction] = useState(null);
 
-  // Sayfalama için öğeleri filtreleme
   const startIndex = (currentPage - 1) * itemsPerPage;
   const paginatedItems = items.slice(startIndex, startIndex + itemsPerPage);
   const totalPages = Math.ceil(items.length / itemsPerPage);
 
-  return (
-    <div>
-      <table className="customer-table">
-        <thead className="customer-table-header">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Payment</th>
-            <th scope="col">Installments</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {paginatedItems.map((item) => (
-            <tr key={item.id}>
-              <td data-label="name" scope="row">
-                <span>{item.customer_name}</span>
-              </td>
-              <td data-label="spending" scope="row">
-                ${item.spending}
-              </td>
-              <td data-label="payment type" scope="row">
-                {item.payment_type}
-              </td>
-              <td data-label="installments left" scope="row">
-                {item.installments_left}
-              </td>
-              <td>
-                <CustomerDetail detailChange={handleDetailChange} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+  const selectedCustomer = items.find(c => c.id === selectedCustomerId);
 
-      {/* Sayfalama bileşeni */}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={setCurrentPage}
-      />
+  return (
+    <div className="customer-table-wrapper">
+      <div className="left-panel">
+        <table className="customer-table">
+          <thead className="customer-table-header">
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Payment</th>
+              <th>Installments</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedItems.map((item) => (
+              <tr key={item.id}>
+                <td>{item.customer_name}</td>
+                <td>${item.spending}</td>
+                <td>{item.payment_type}</td>
+                <td>{item.installments_left}</td>
+                <td>
+                  <CustomerDetail
+                    onSelectDetail={(action) => {
+                      setSelectedCustomerId(item.id);
+                      setSelectedAction(action);
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+
+      <div className={`right-panel ${selectedCustomer && selectedAction ? 'open' : ''}`}>
+        {selectedAction === 'Personal Information' && selectedCustomer && (
+          <PersonalInfo customer={selectedCustomer} />
+        )}
+      </div>
     </div>
   );
 };
