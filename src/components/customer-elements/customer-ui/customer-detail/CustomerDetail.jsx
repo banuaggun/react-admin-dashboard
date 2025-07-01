b10b0f21d3984c451c989622f0c11fc006950ba4
Banu Ağgün <banuaggun@outlook.com>
2025-07-02 00:20:26 +0300

    customer detail created

--- src/components/customer-elements/customer-ui/customer-detail/CustomerDetail.jsx
+++ src/components/customer-elements/customer-ui/customer-detail/CustomerDetail.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import Dropdown from '../../../../functions/dropdown/Dropdown'
-import { data } from 'react-router-dom'
-import useFetchData from '../../../../functions/hooks/FetchData'
+import React from 'react';
+import Dropdown from '../../../../functions/dropdown/Dropdown';
+import useFetchData from '../../../../functions/hooks/FetchData';
 
-const CustomerDetail = () => {
-  const {data, loading, error} = useFetchData('/api/info.json');
+const CustomerDetail = ({ onSelectDetail }) => {
+  const { data, loading, error } = useFetchData('/api/info.json');
+
   if (loading) return <p>Yükleniyor...</p>;
   if (error) return <p>Hata: {error.message}</p>;
+
+  const dropdownItems = data?.details?.map((detail) => ({
+    name: detail.name || "Unknown",
+    onClick: () => onSelectDetail(detail.name) // sadece tıklamada aksiyon çalışıyor
+  })) || [];
+
   return (
-    <div className='filter'>
+    <div className="filter">
       <Dropdown
         icon={
           <div className="icon-container">
             <i className="ph-fill ph-dots-three-circle icon-detail"></i>
           </div>
         }
-        items={data?.details?.map((detail) => ({
-          name: detail.name || "Unknown Filter"
-        })) || []} // Eğer filtreler boşsa varsayılan boş array
+        items={dropdownItems}
       />
     </div>
-  )
-}
+  );
+};
 
-export default CustomerDetail
+export default CustomerDetail;
