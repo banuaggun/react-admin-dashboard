aa19b91738a22ebf8a4c5143729d0b50a9ea1ada
Banu Ağgün <banuaggun@outlook.com>
2025-07-03 13:11:49 +0300

    column selector property is defined in customer table

--- src/components/customer-elements/customer-ui/customer-table/CustomerTable.jsx
+++ src/components/customer-elements/customer-ui/customer-table/CustomerTable.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import CustomerDetail from "../customer-detail/CustomerDetail";
 import Pagination from "../pagination/Pagination";
 import PersonalInfo from "../customer-actions/PersonalInfo";
-import './customer-table.css';
+import "./customer-table.css";
+import ColumnSelector from "../column-selector/ColumnSelector";
 
 const CustomerTable = ({ items }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
-
   const [selectedCustomerId, setSelectedCustomerId] = useState(null);
   const [selectedAction, setSelectedAction] = useState(null);
+  const [selectedColumn, setSelectedColumn] = useState("amount");
 
+  const itemsPerPage = 5;
   const startIndex = (currentPage - 1) * itemsPerPage;
   const paginatedItems = items.slice(startIndex, startIndex + itemsPerPage);
   const totalPages = Math.ceil(items.length / itemsPerPage);
-
-  const selectedCustomer = items.find(c => c.id === selectedCustomerId);
+  const selectedCustomer = items.find((c) => c.id === selectedCustomerId);
 
   return (
     <div className="customer-table-wrapper">
@@ -24,9 +24,12 @@ const CustomerTable = ({ items }) => {
           <thead className="customer-table-header">
             <tr>
               <th>Name</th>
-              <th>Amount</th>
-              <th>Payment</th>
-              <th>Installments</th>
+              <th>
+                <ColumnSelector
+                  selectedColumn={selectedColumn}
+                  onSelectColumn={setSelectedColumn}
+                />
+              </th>
               <th></th>
             </tr>
           </thead>
@@ -34,9 +37,11 @@ const CustomerTable = ({ items }) => {
             {paginatedItems.map((item) => (
               <tr key={item.id}>
                 <td>{item.customer_name}</td>
-                <td>${item.spending}</td>
-                <td>{item.payment_type}</td>
-                <td>{item.installments_left}</td>
+                <td colSpan={3}>
+                  {selectedColumn === "amount" && `$${item.spending}`}
+                  {selectedColumn === "payment" && item.payment_type}
+                  {selectedColumn === "installment" && item.installments_left}
+                </td>
                 <td>
                   <CustomerDetail
                     onSelectDetail={(action) => {
@@ -57,8 +62,12 @@ const CustomerTable = ({ items }) => {
         />
       </div>
 
-      <div className={`right-panel ${selectedCustomer && selectedAction ? 'open' : ''}`}>
-        {selectedAction === 'Personal Information' && selectedCustomer && (
+      <div
+        className={`right-panel ${
+          selectedCustomer && selectedAction ? "open" : ""
+        }`}
+      >
+        {selectedAction === "Personal Information" && selectedCustomer && (
           <PersonalInfo customer={selectedCustomer} />
         )}
       </div>
