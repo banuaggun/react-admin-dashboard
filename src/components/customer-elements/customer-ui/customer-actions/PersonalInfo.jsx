ffe7df4ad7641c6e4ac7a5986d1476f2363b42d6
Banu Ağgün <banuaggun@outlook.com>
2025-07-18 16:59:57 +0300

    Personal info information has been edited

--- src/components/customer-elements/customer-ui/customer-actions/PersonalInfo.jsx
+++ src/components/customer-elements/customer-ui/customer-actions/PersonalInfo.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import './personal.css';
+import React from "react";
+import "./personal.css";
 
-const PersonalInfo = ({ customer }) => {
+const PersonalInfo = ({ customer, onClose }) => {
   if (!customer) {
     return <p>Seçili müşteri bilgisi bulunamadı.</p>;
   }
 
   const fields = [
-  { label: "Email", value: customer.email },
-  { label: "Telefon", value: customer.phone },
-  { label: "Ödeme Türü", value: customer.payment_type },
-  { label: "Taksit", value: customer.installments_left }
-];
+    { label: "Email", value: customer.email },
+    { label: "Phone", value: customer.phone },
+    { label: "Payment Method", value: customer.payment_type },
+    { label: "Number of Installments", value: customer.installments_left },
+  ];
 
   return (
-      <div className="personal-info-card">
-    <h4>{customer.customer_name || "İsim bilinmiyor"}</h4>
-    {fields.map((field, index) => (
-      <p key={index}><strong>{field.label}:</strong> {field.value ?? "Tanımsız"}</p>
-    ))}
-  </div>
+    <div className="personal-info-card">
+      <div className="personal-info-card-row-1">
+        <button>
+          <i className="ph-fill ph-pencil-line"></i>
+          <span>Edit</span>
+        </button>
+        <span onClick={onClose} style={{ cursor: "pointer" }}>
+          <i className="ph-fill ph-x-circle"></i>
+        </span>
+      </div>
+
+      <div className="personal-info-card-row-2">
+        <h4>{customer.customer_name || "İsim bilinmiyor"}</h4>
+        {fields.map((field, index) => (
+          <div className="customer-info" key={index}>
+            <h5>{field.label}</h5>
+
+            <p> {field.value ?? "Tanımsız"}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="personal-info-card-row-3">
+        <button>
+          <i className="ph-fill ph-trash"></i>
+          <span>Delete</span>
+        </button>
+      </div>
+    </div>
   );
 };
-
 export default PersonalInfo;
