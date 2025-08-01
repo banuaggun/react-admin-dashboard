f96a4199086ff9b418ff103d04d102b6c3bb89f5
Banu Ağgün <banuaggun@outlook.com>
2025-08-01 11:41:31 +0300

    reorganized location and style properties - 2

--- src/components/customer-elements/customer-ui/customer-actions/PersonalInfo.jsx
+++ src/components/customer-elements/customer-ui/customer-actions/PersonalInfo.jsx
@@ -15,32 +15,33 @@ const PersonalInfo = ({ customer, onClose }) => {
 
   return (
     <div className="personal-info-card">
-      <div className="personal-info-card-row-1">
-        <button>
-          <i className="ph-fill ph-pencil-line"></i>
-          <span>Edit</span>
-        </button>
-        <span onClick={onClose} style={{ cursor: "pointer" }}>
-          <i className="ph-fill ph-x-circle"></i>
-        </span>
-      </div>
+      <div className="personal-info-card-row">
+        <div className="personal-info-card-row-1">
+          <button>
+            <i className="ph-fill ph-pencil-line"></i>
+            <span>Edit</span>
+          </button>
+          <span onClick={onClose} style={{ cursor: "pointer" }}>
+            <i className="ph-fill ph-x-circle"></i>
+          </span>
+        </div>
 
-      <div className="personal-info-card-row-2">
-        <h4>{customer.customer_name || "İsim bilinmiyor"}</h4>
-        {fields.map((field, index) => (
-          <div className="customer-info" key={index}>
-            <h5>{field.label}</h5>
+        <div className="personal-info-card-row-2">
+          <input className="name" value={customer.customer_name || "İsim bilinmiyor"} />
+          {fields.map((field, index) => (
+            <div className="customer-info" key={index}>
+              <h5>{field.label}</h5>
+              <input value={field.value ?? "Tanımsız"} />
+            </div>
+          ))}
+        </div>
 
-            <p> {field.value ?? "Tanımsız"}</p>
-          </div>
-        ))}
-      </div>
-
-      <div className="personal-info-card-row-3">
-        <button>
-          <i className="ph-fill ph-trash"></i>
-          <span>Delete</span>
-        </button>
+        <div className="personal-info-card-row-3">
+          <button>
+            <i className="ph-fill ph-trash"></i>
+            <span>Delete</span>
+          </button>
+        </div>
       </div>
     </div>
   );
